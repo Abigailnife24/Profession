ed24762a4a0a2b6cf7f5fc09534f9622af15d0d6
@Oluyede1 <nife.oluyede@gmail.com>
2024-10-21 22:57:09 +0100

    new commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const Ali = {
     requiredYears: 10,
     totalHoursRequired: 0,
      daysInAYear : 365,
+     yearSpentInSchool: 15,
     calculateTotalHours: function () {
         this.totalHoursRequired = this.requiredHours * this.daysInAYear * this.requiredYears;
     },
@@ -37,3 +38,25 @@ const result = Ali.successOrFailure(hours);
 
 alert(result);
 
+const counselAli = (hoursWorked, yearSpentInSchool) =>{
+    const requiredHours = 8 * 365 * 10; // Assuming 8 hours/day, 365 days/year, 10 years
+    const isSuccessful = hoursWorked >= requiredHours;
+
+    if(yearSpentInSchool > 20) {
+        if (isSuccessful) {
+            return"Congratulations! Your perseverance has paid off. Keep shining!"
+        }
+        else{
+            return"Don't hesitate - reach out for support now!"
+        }
+    }
+
+    else{
+        if(isSuccesful) {
+            return"You did it! Prioritize your well-being now."
+        }
+        else{
+            return "You're doing great, keep going!"
+        }
+    }
+}
